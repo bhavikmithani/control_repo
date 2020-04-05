c55ca1ba9c2423b37c979af8138dc3050f778e79
bhavikmithani <bhavikmithani@hotmail.com>
2020-04-05 14:21:01 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCdhWOFOQV1RLC/uiw9/07xlKkKVmR4FPe80Ohj/9cwF0ZpoM6H1NlAj7+L222TeRKQESeEs4WUzOF51/qOeniEkpv+mp9+t+jQntfbwidE7rM9bwA7TbOPvCANcdc9FN3LYDlZZPK7tM5ut2UKL4Ew+px1dwiCCaGXeQr5p9NvF6r0dySKSLMuAGU0OMj2/9PVGiCKf+1aPmrq2rlEMEM/svN6/xGAdPGaBk+/AchqIxmZhioPIlNVz19JVhwG/0PscM2wmrRi1fZmVIdzehLZFcD5cBx2S0YZXfeMXGK5OdQW73QwiVl8XeLv2k45HCiVRXH2WhCPEIhMog+EXV5t',
 	}  
 }
